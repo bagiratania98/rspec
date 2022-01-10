2cbc09e781862e1632458dbb162e98325c79e0da
Bagira Tania <bagiratana@gmail.com>
2022-01-10 20:24:47 +0200

    Add User

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,12 @@ module Everydayrspec
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
